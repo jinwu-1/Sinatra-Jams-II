e6d8fbdba9c2ad5bb0b6270987124b93eeacbb63
Jin Wu <jinn.wuu@gmail.com>
2020-01-30 10:47:57 -0500

    semi-done

---
+++ db/migrate/20200130142011_create_artist_instruments.rb
@@ -0,0 +1,10 @@
+class CreateArtistInstruments < ActiveRecord::Migration
+  def change
+    create_table :artist_instruments do |t|
+      t.integer :artist_id
+      t.integer :instrument_id
+
+      t.timestamp
+    end
+  end
+end
